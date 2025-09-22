d526ec63cd8e41ef1abea60406408b6046c8fc7a
CardosoB8 <166424120+CardosoB8@users.noreply.github.com>
2025-09-22 18:32:25 +0200

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -1,1 +1,31 @@
 
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "api/process.js",
+      "use": "@vercel/node"
+    },
+    {
+      "src": "public/**/*",
+      "use": "@vercel/static"
+    }
+  ],
+  "routes": [
+    {
+      "src": "/api/(.*)",
+      "dest": "/process.js"
+    },
+    {
+      "src": "/(.*)",
+      "dest": "/public/$1"
+    },
+    {
+      "handle": "filesystem"
+    },
+    {
+      "src": "/(.*)",
+      "dest": "/process.js"
+    }
+  ]
+}
